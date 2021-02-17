a0762b995c2f767565ac44d8a811395534d3f677
sukeshchand <sukeshchand@hotmail.com>
2021-02-17 12:22:24 +0530

    content script injected

---
+++ content_script.js
@@ -0,0 +1,1 @@
+alert($("#timesheet-data-island").text());
---
+++ popup.js
@@ -0,0 +1,13 @@
+let changeColor = document.getElementById('changeColor');
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'alert("testttt");'});
+  });
+};
